5b45b7832ca8ceb18c5da5e144f23d3af64a891d
Dave Ortiz <daveortiz@gmail.com>
2013-08-15 14:55:15 -0400

    works

---
+++ deploy/node-war/config/deploy/group2.rb
@@ -0,0 +1,31 @@
+# TOMCAT SERVERS
+role :webserver, 
+     "carra-node-21-production",
+     "carra-node-22-production",
+     "carra-node-23-production",
+     "carra-node-24-production",
+     "carra-node-25-production",
+     "carra-node-26-production",
+     "carra-node-27-production",
+     "carra-node-28-production",
+     "carra-node-29-production",
+     "carra-node-30-production",
+     "carra-node-31-production",
+     "carra-node-32-production",
+     "carra-node-33-production",
+     "carra-node-34-production",
+     "carra-node-35-production",
+     "carra-node-36-production",
+     "carra-node-37-production",
+     "carra-node-38-production",
+     "carra-node-39-production",
+     "carra-node-40-production"
+
+namespace :carra do
+  desc "Push out shrine-config-production.xml"
+  task :pushconfig do
+    run "mkdir -p #{tomcat_home}conf/Catalina/localhost/"
+    file = File.read("config/shrine-config-production.xml")
+    put file, "#{tomcat_home}conf/Catalina/localhost/shrine-cell.xml"
+  end
+end
